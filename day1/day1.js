6032adcac4638896cd3470627c504a7c62b64fb8
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-19 23:30:37 +0500

    added some changes

--- day1/day1.js
+++ day1/day1.js
@@ -1,10 +1,10 @@
 const age = 14;
-const name = "sheraz";
+const hasVoterCard = "yes";
 
-if (age == 13) {
-  document.write("he can vote");
-} else if (age < 14) {
-  document.write("he should apply for vote");
-} else {
-  document.write("voter has been died");
+if (age >= 18 && hasVoterCard === "yes") {
+  console.log("You can vote");
+} else if (age >= 18 && hasVoterCard !== "yes") {
+  console.log("Apply for a voter card");
+} else if (age < 18) {
+  console.log("You are not eligible to vote");
 }
