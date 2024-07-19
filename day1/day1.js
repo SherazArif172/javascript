12247c0813f8b7b1536fb17b41539007f797cca1
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-19 18:47:24 +0500

    my first commit

---
+++ day1/day1.js
@@ -0,0 +1,10 @@
+const age = 14;
+const name = "sheraz";
+
+if (age == 13) {
+  document.write("he can vote");
+} else if (age < 14) {
+  document.write("he should apply for vote");
+} else {
+  document.write("voter has been died");
+}
