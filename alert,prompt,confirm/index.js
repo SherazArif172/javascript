01202451e2ee25bb9d94050e39b1497bfd360ba4
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-25 01:05:54 +0500

    added confirm

--- alert,prompt,confirm/index.js
+++ alert,prompt,confirm/index.js
@@ -1,6 +1,6 @@
 //alert
 
-alert("are tou sure ?");
+alert("error is here ");
 
 // prompt
 
@@ -10,4 +10,12 @@ if (age == null) {
   document.write("age is not full filled");
 } else {
   document.write(`your age is ${age}`);
+}
+
+//confirm
+
+let confirmed = confirm("are you sure ?");
+
+if (confirmed) {
+  document.write("get lost");
 }
