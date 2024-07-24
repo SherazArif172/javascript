b9495c45a991dc26172b1473300cf68322467682
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-25 01:04:15 +0500

    added alert and prompt

---
+++ alert,prompt,confirm/index.js
@@ -0,0 +1,13 @@
+//alert
+
+alert("are tou sure ?");
+
+// prompt
+
+let age = prompt("put you age");
+
+if (age == null) {
+  document.write("age is not full filled");
+} else {
+  document.write(`your age is ${age}`);
+}
